5997722839143e2e197c1b186a04d0b573a29ca3
marcosborg <m.borges.mail@gmail.com>
2025-02-12 12:20:22 +0000

    Update topbar to use dynamic social media links from WebsiteConfiguration

--- resources/views/components/website/topbar.blade.php
+++ resources/views/components/website/topbar.blade.php
@@ -10,12 +10,11 @@
 
         <div class="topbar-right">
             <div class="social-icons">
-                <a href="#"><i class="fa fa-facebook fa-lg"></i></a>
-                <a href="#"><i class="fa fa-instagram fa-lg"></i></a>
-                <a href="#"><i class="fa fa-whatsapp fa-lg"></i></a>
+                <a href="{{ $website_configuration->facebook }}"><i class="fa fa-facebook fa-lg"></i></a>
+                <a href="{{ $website_configuration->instagram }}"><i class="fa fa-instagram fa-lg"></i></a>
+                <a href="https://wa.me/{{ $website_configuration->whatsapp }}" target="_new"><i class="fa fa-whatsapp fa-lg"></i></a>
             </div>
         </div>
         <div class="clearfix"></div>
     </div>
 </div>
-<script>console.log({!! $website_configuration !!})</script>
