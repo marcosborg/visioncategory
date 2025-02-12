e329b9ad442d5c88cc8ca92e23c20917e0f0a9c6
marcosborg <m.borges.mail@gmail.com>
2025-02-12 13:22:56 +0000

    Refactor topbar component to fetch website configuration data directly from the model

--- resources/views/components/website/topbar.blade.php
+++ resources/views/components/website/topbar.blade.php
@@ -2,17 +2,17 @@
     <div class="container">
         <div class="topbar-left xs-hide">
             <div class="topbar-widget">
-                <div class="topbar-widget"><a href="#"><i class="fa fa-phone"></i>{{ $website_configuration->phone }}</a></div>
-                <div class="topbar-widget"><a href="#"><i class="fa fa-envelope"></i>{{ $website_configuration->email }}</a>
+                <div class="topbar-widget"><a href="#"><i class="fa fa-phone"></i>{{ \App\Models\WebsiteConfiguration::find(1)->phone }}</a></div>
+                <div class="topbar-widget"><a href="#"><i class="fa fa-envelope"></i>{{ \App\Models\WebsiteConfiguration::find(1)->email }}</a>
                 </div>
             </div>
         </div>
 
         <div class="topbar-right">
             <div class="social-icons">
-                <a href="{{ $website_configuration->facebook }}"><i class="fa fa-facebook fa-lg"></i></a>
-                <a href="{{ $website_configuration->instagram }}"><i class="fa fa-instagram fa-lg"></i></a>
-                <a href="https://wa.me/{{ $website_configuration->whatsapp }}" target="_new"><i class="fa fa-whatsapp fa-lg"></i></a>
+                <a href="{{ \App\Models\WebsiteConfiguration::find(1)->facebook }}"><i class="fa fa-facebook fa-lg"></i></a>
+                <a href="{{ \App\Models\WebsiteConfiguration::find(1)->instagram }}"><i class="fa fa-instagram fa-lg"></i></a>
+                <a href="https://wa.me/{{ \App\Models\WebsiteConfiguration::find(1)->whatsapp }}" target="_new"><i class="fa fa-whatsapp fa-lg"></i></a>
             </div>
         </div>
         <div class="clearfix"></div>
